ccb1865db1d3dab44dd643544e4f5e78839644ca
an-nikita <nikita.b@applicationnexus.com>
2025-02-06 20:54:52 +0530

    Angular reactive form task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FirstComponent} from './first/first.component'
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FirstComponent,ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-first',
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent {
+
+
+
+  registerForm = new FormGroup({
+    fullName: new FormControl('', Validators. required),
+    username: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+    acceptTerms: new FormControl(false, Validators.requiredTrue)
+  });
+
+ 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
+  }
+}
