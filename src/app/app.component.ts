9eef9adbaf6e2087d8e3f580f10bef11a773aaab
an-nikitabhagat <nikita.b@applicationnexus.com>
2025-02-10 12:47:04 +0530

    successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecondComponent } from './second/second.component';
+
+import { ThirdComponent } from './third/third.component';
 import { FirstComponent} from './first/first.component'
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,7 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponent,ReactiveFormsModule,FormsModule,CommonModule,SecondComponent],
+  imports: [RouterOutlet,FirstComponent,ReactiveFormsModule,FormsModule,CommonModule,SecondComponent,ThirdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
